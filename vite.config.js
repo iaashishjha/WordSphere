cb6cfdfc4cddb65f5ac22a66c4fdcc10ed58fb15
Aashish Jha <er.aashishjha@gmail.com>
2025-09-16 00:37:34 +0530

    Final fix for Vercel asset

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', 
+ build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  base: '',
   plugins: [react(), tailwindcss(),],
   
  
